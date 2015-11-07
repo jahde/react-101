45a81d31a84ae0e22c4c119c786cee9b914974b1
jahde <jahde@classpass.com>
2015-11-07 16:50:44 -0500

    first commit

--- subjects/00-hello-world/exercise.js
+++ subjects/00-hello-world/exercise.js
@@ -1,12 +1,80 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+// ES5 var App = React.createClass({...})
+
+// ES6
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			myHeading: 'Hello Universe',
+			changeColor: false,
+			myNumber: 0
+		};
+		this.changeColor = this.changeColor.bind(this);
+		this.addOne = this.addOne.bind(this);
+	}
+
+	changeColor() {
+		this.setState({changeColor: !this.state.changeColor});
+	}
+
+	// addOne = () => {        Do not have to bind manually
+	//	this.state.myNumber++;
+	//	this.setState();
+	// }
+	addOne() {
+		this.setState({myNumber: this.state.myNumber + 1});
+	}
+
+	componentWillMount() {
+		console.log('Will mount');
+	}
+
+	componentDidMount() {
+		console.log('Did mount');
+	}
+
+	componentWillUpdate() {
+		console.log('Will update');
+	}
+
+	componentDidUpdate() {
+		console.log('Did update');
+	}
 
   render() {
-    return <h1>Hello World!</h1>
+    return (
+    	<div>
+    		<h1>Hello World!</h1>
+    		<h2 style={this.state.changeColor ? {backgroundColor: 'red'} : {backgroundColor: 'yellow'}}>{ this.state.myHeading }</h2>
+    		<p> {this.state.myNumber} </p>
+    		<span style={{fontSize: '30px'}}> {this.props.name} </span>
+    		<button onClick={this.addOne}> Add One </button>
+    		<button onClick={this.changeColor}> Click me </button>
+    	</div>
+    )
   }
 
 }
 
-render(<App />, document.getElementById('app'))
+class Parent extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {myState: 'Parent state'};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> My second React component </h1>
+				<App name={this.state.myState}/>
+				<App name={'App 2'}/>
+				<App name={'App 3'}/>
+			</div>
+			)
+	}
+}
+
+render(<Parent />, document.getElementById('app'))
--- subjects/01-imperative-to-declarative/lecture.js
+++ subjects/01-imperative-to-declarative/lecture.js
@@ -14,17 +14,108 @@ styles.theremin = {
   display: 'inline-block'
 }
 
-class App extends React.Component {
+class Tone extends React.Component {
   constructor(props) {
     super(props)
+    this.doImperativeWork = this.doImperativeWork.bind(this);
+  }
+
+  componentWillMount() {
+      this.theremin = createTheremin();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+      this.doImperativeWork();
+  }
+
+  doImperativeWork() {
+    if (this.props.isPlaying) {
+      this.theremin.play();
+    } else {
+      this.theremin.stop()
+    }
+    this.theremin.setPitchBend(this.props.pitch);
+    this.theremin.setVolume(this.props.volume);
+    this.theremin.setType(this.props.type);
+  }
+
+  render(){
+    return null;
+  }
+}
+
+class Theremin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isPlaying: false,
+      pitch: 0,
+      volume: 0,
+      type: 'sine'
+    }
+    // this.play = this.play.bind(this);
+    // this.stop = this.stop.bind(this);
+  }
+
+  componentWillMount() {
+    this.theremin = createTheremin();
+  }
+
+  // play() {
+  //   this.theremin.play()
+  // }
+
+  play = () => {
+    this.setState({isPlaying: true});
+    // this.theremin.play()
+  }
+
+  // stop() {
+  //   this.theremin.stop()
+  // }
+
+  stop = () => {
+    this.setState({isPlaying: false});
+    this.theremin.stop()
+  }
+
+  changeTone = (event) => {
+    let { clientX, clientY } = event;
+    let { top, bottom, left, right } = event.target.getBoundingClientRect();
+    let pitch = (clientX - left) / right;
+    let volume = 1 - (clientY - top) / bottom;
+
+    this.setState( {pitch: pitch, volume: volume} );
+    // this.theremin.setPitchBend(pitch);
+    // this.theremin.setVolume(volume);
   }
 
   render() {
     return (
       <div>
-        <h1> What does it mean to be declarative? </h1>
+        <div style={styles.theremin}
+             onMouseEnter={this.play}
+             onMouseLeave={this.stop}
+             onMouseMove={this.changeTone}/>
+            <Tone isPlaying={this.state.isPlaying}
+                  pitch={this.state.pitch}
+                  volume={this.state.volume}
+                  type={this.props.type}/>
       </div>
       );
+  }
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1> What does it mean to be declarative? </h1>
+        <Theremin type='sine' />
+        <Theremin type='square' />
+        <Theremin type='sawtooth' />
+      </div>
+      )
   }
 }
 
